ce93c7bc768b1e093fc92b5ce292c1b5533ce9dc
kurs <kurs@10.0.2.15>
2017-12-03 09:40:01 +0100

    Druga

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,8 +3,9 @@ package hello;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello world");
+		System.out.println("Pierwsza wersja");
+		System.out.println("Druga wersja");
 	}
 
 }
